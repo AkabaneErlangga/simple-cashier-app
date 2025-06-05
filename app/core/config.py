624876e6bf5222d6a211d2c0214b5d13a84ffc7f
Er <ursawarrior157@gmail.com>
2025-06-05 15:50:02 +0700

    fix: env

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 from pydantic import EmailStr, HttpUrl, PostgresDsn, computed_field
 from pydantic_core import MultiHostUrl
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -9,15 +9,17 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 days
     PROJECT_NAME: str = "Cashier App"
     SENTRY_DSN: HttpUrl | None = None
-    POSTGRES_SERVER: str = "43.133.132.163"
-    POSTGRES_PORT: int = 31067
-    POSTGRES_USER: str = "root"
-    POSTGRES_PASSWORD: str = "fZ86lhA13HP57W4k9xTEpdJuBiON20Fr"
+    POSTGRES_SERVER: str 
+    POSTGRES_PORT: int 
+    POSTGRES_USER: str 
+    POSTGRES_PASSWORD: str 
     POSTGRES_DB: str = "cashier-app"
 
     EMAIL_TEST_USER: EmailStr = "root@akabane.my.id"
     FIRST_SUPERUSER: EmailStr = "root@akabane.my.id"
     FIRST_SUPERUSER_PASSWORD: str = "sakura123"
+
+    model_config = SettingsConfigDict(env_file=".env")
 
     @computed_field  # type: ignore[prop-decorator]
     @property
